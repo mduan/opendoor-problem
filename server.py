2ccc3590ba3a838e45f60ad5a64a012f0ac9d735
Mack Duan <duan.mack@gmail.com>
2015-11-16 19:48:15 -0800

    add pagination

--- server.py
+++ server.py
@@ -5,24 +5,35 @@ import requests
 
 app = flask.Flask(__name__)
 
+def str_to_int(str_, default):
+  return int(str_) if str_ else default
+
 @app.route('/listings', methods=['GET'])
 def listings():
   args = flask.request.args
-  min_price = int(args.get('min_price')) or float('-inf')
-  max_price = int(args.get('max_price')) or float('inf')
-  min_bed = int(args.get('min_bed')) or float('-inf')
-  max_bed = int(args.get('max_bed')) or float('inf')
-  min_bath = int(args.get('min_bath')) or float('-inf')
-  max_bath = int(args.get('max_bath')) or float('inf')
+
+  min_price = str_to_int(args.get('min_price'), float('-inf'))
+  max_price = str_to_int(args.get('max_price'), float('inf'))
+  min_bed = str_to_int(args.get('min_bed'), float('-inf'))
+  max_bed = str_to_int(args.get('max_bed'), float('inf'))
+  min_bath = str_to_int(args.get('min_bath'), float('-inf'))
+  max_bath = str_to_int(args.get('max_bath'), float('inf'))
+  page = str_to_int(args.get('page'), 0)
+  results_per_page = str_to_int(args.get('results_per_page'), 100)
 
   house_listings = []
-  for house in app.config.houses:
+  start_idx = page * results_per_page
+  end_idx = (page + 1) * results_per_page
+  for curr_idx, house in enumerate(app.config.houses):
+    if curr_idx >= end_idx: # early terminate when we have enough results for current page
+      break
     price = house['price']
     num_beds = house['bedrooms']
     num_baths = house['bathrooms']
     if (price >= min_price and price <= max_price and
         num_beds >= min_bed and num_beds <= max_bed and
-        num_baths >= min_bath and num_baths <= max_bath):
+        num_baths >= min_bath and num_baths <= max_bath and
+        curr_idx >= start_idx and curr_idx < end_idx):
       house_listings.append({
         'type': 'Feature',
         'geometry': {
@@ -38,6 +49,7 @@ def listings():
           'sq_ft': house['sq_ft'],
         },
       })
+      print curr_idx
 
   response = {
     'type': 'FeatureCollection',
@@ -70,6 +82,7 @@ def fetch_houses():
   return houses
 
 if __name__ == '__main__':
+  app.debug = True
   app.config.houses = fetch_houses()
   port = int(os.environ.get('PORT', 5000))
   app.run(host='0.0.0.0', port=port)
